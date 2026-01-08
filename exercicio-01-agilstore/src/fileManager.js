0cd207592e033232c8de6173458d33478fba4514
Caputi <lcaputi2@gmail.com>
2026-01-08 19:19:47 -0300

    feat: finish lerDados function

--- exercicio-01-agilstore/src/fileManager.js
+++ exercicio-01-agilstore/src/fileManager.js
@@ -1,4 +1,15 @@
+const fs = require('fs');
+
+const caminhoListaProdutos = '../data/inventario.json';
 
 function lerDados(){
-    
+    try{
+        const dadosBrutos = fs.readFileSync(caminhoListaProdutos, 'utf8');
+        const dadosJSON = JSON.parse(dadosBrutos);
+
+        return dadosJSON;
+
+    }catch(err){
+        console.error('Erro ao ler o arquivo:', err);
+    }
 }
