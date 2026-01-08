8f57f4c9bfa0a83ef8a541e1caca6e65e45f5f05
Caputi <lcaputi2@gmail.com>
2026-01-08 19:53:58 -0300

    chore: minor changes at file manager

--- exercicio-01-agilstore/src/fileManager.js
+++ exercicio-01-agilstore/src/fileManager.js
@@ -4,9 +4,7 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 const CAMINHO_ARQUIVO = path.join(__dirname, '../data/inventario.json');
-
 
 export async function lerDados() {
     try {
@@ -16,18 +14,11 @@ export async function lerDados() {
         if (erro.code === 'ENOENT') {
             return [];
         }
-        console.error('Erro desconhecido ao ler dados:', erro);
         throw erro;
     }
 }
 
-export async function salvarDados(produtos) {
-    try {
-        const conteudo = JSON.stringify(produtos, null, 2);
-        
-        await fs.writeFile(CAMINHO_ARQUIVO, conteudo, 'utf-8');
-    } catch (erro) {
-        console.error('Erro ao salvar os dados:', erro);
-        throw erro;
-    }
+export async function salvarDados(dados) {
+    const conteudo = JSON.stringify(dados, null, 2);
+    await fs.writeFile(CAMINHO_ARQUIVO, conteudo, 'utf-8');
 }
