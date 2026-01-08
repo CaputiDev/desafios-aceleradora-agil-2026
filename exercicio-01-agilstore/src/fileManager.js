5093dd5c50c07dd9fbe0f4c07b0468260ced85a8
Caputi <lcaputi2@gmail.com>
2026-01-08 19:40:33 -0300

    feat: implements fileManager stable version

--- exercicio-01-agilstore/src/fileManager.js
+++ exercicio-01-agilstore/src/fileManager.js
@@ -1,15 +1,33 @@
-const fs = require('fs');
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const caminhoListaProdutos = '../data/inventario.json';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-function lerDados(){
-    try{
-        const dadosBrutos = fs.readFileSync(caminhoListaProdutos, 'utf8');
-        const dadosJSON = JSON.parse(dadosBrutos);
+const CAMINHO_ARQUIVO = path.join(__dirname, '../data/inventario.json');
 
-        return dadosJSON;
 
-    }catch(err){
-        console.error('Erro ao ler o arquivo:', err);
+export async function lerDados() {
+    try {
+        const dados = await fs.readFile(CAMINHO_ARQUIVO, 'utf-8');
+        return JSON.parse(dados);
+    } catch (erro) {
+        if (erro.code === 'ENOENT') {
+            return [];
+        }
+        console.error('Erro desconhecido ao ler dados:', erro);
+        throw erro;
+    }
+}
+
+export async function salvarDados(produtos) {
+    try {
+        const conteudo = JSON.stringify(produtos, null, 2);
+        
+        await fs.writeFile(CAMINHO_ARQUIVO, conteudo, 'utf-8');
+    } catch (erro) {
+        console.error('Erro ao salvar os dados:', erro);
+        throw erro;
     }
 }
